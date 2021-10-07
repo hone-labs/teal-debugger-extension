1bafff725fe9a9980b36311c7d5b51286d599518
Ashley Davis <ashley@codecapers.com.au>
2021-10-08 09:10:15 +1000

    New TEAL example and updates to the debugger to be able to support it. The configuration of the "mock data" for the debugger is read from a configuration file that sits next to the teal file.

--- src/teal-runtime.ts
+++ src/teal-runtime.ts
@@ -2,9 +2,12 @@
 // A simplified wrapper for the teal runtime.
 //
 
-import { Interpreter, parser, types } from "@algo-builder/runtime";
-import { setgroups } from "process";
+import { AccountStore, Interpreter, parser, Runtime, types } from "@algo-builder/runtime";
+import { types as webTypes } from "@algo-builder/web";
+import { Ctx } from "@algo-builder/runtime/build/ctx";
+import { ALGORAND_ACCOUNT_MIN_BALANCE } from "@algo-builder/runtime/build/lib/constants";
 import { readFile } from "./lib/file";
+import cloneDeep from "lodash.clonedeep";
 
 export class TealRuntime {
 
@@ -64,10 +67,43 @@ export class TealRuntime {
         this.loadedTealFilePath = tealFilePath;
         this.nextInstructionIndex = 0;
 
-        const contents = await readFile(tealFilePath);
+        const tealCode = await readFile(tealFilePath);
+        const configuration = JSON.parse(await readFile(tealFilePath + ".json"));
+
+        let accountMap = new Map<string, AccountStore>();
+        let accounts: AccountStore[] = [];
+        for (let accountName of Object.keys(configuration.accounts)) {
+            const accountData = configuration.accounts[accountName];
+            const account = new AccountStore(accountData.balance, accountData);
+            accountMap.set(accountName, account);
+            accounts.push(account);
+        }
+        
+        const runtime = new Runtime(accounts);
+        const lsig = runtime.getLogicSig(tealCode, []);
+        const lsigAccount = runtime.getAccount(lsig.address());
+        accountMap.set("$lsig", lsigAccount);
+
+        const txnParameters: any = configuration.transactionParams;
+        if (txnParameters.fromAccount) {
+            txnParameters.fromAccountAddr = accountMap.get(txnParameters.fromAccount)?.address;
+            delete txnParameters.fromAccount;
+        }
+
+        if (txnParameters.toAccount) {
+            txnParameters.toAccountAddr = accountMap.get(txnParameters.toAccount)?.address;
+            delete txnParameters.toAccount;
+        }
+
+        const [tx, gtxs] = runtime.createTxnContext([txnParameters]);
+
+        const store = (runtime as any).store; //TODO: This is a bit ugly. Need to make a change to algo-builder/runtime if we need to keep this.
+        runtime.ctx = new Ctx(cloneDeep(store), tx, gtxs, [], runtime, undefined);
 
         this.interpreter = new Interpreter();
-        this.instructions = parser(contents, types.ExecutionMode.APPLICATION, this.interpreter);
+        this.interpreter.runtime = runtime;
+        this.instructions = parser(tealCode, types.ExecutionMode.SIGNATURE, this.interpreter);
+        this.interpreter.mapLabelWithIndexes();
     }
 
     //
