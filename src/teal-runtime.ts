6444a4c46f80c15e1dac533d6660ac0b8e74069f
Ashley Davis <ashley@codecapers.com.au>
2021-10-28 08:23:09 +1000

    Continued to flesh out the configuration file format.

--- src/teal-runtime.ts
+++ src/teal-runtime.ts
@@ -2,12 +2,13 @@
 // A simplified wrapper for the teal runtime.
 //
 
-import { AccountStore, Interpreter, parser, Runtime, types } from "@algo-builder/runtime";
-import { types as webTypes } from "@algo-builder/web";
+import { AccountStore, Interpreter, LogicSigAccount, parser, Runtime, types } from "@algo-builder/runtime";
+import { types as webTypes, parsing } from "@algo-builder/web";
 import { Ctx } from "@algo-builder/runtime/build/ctx";
 import { readFile } from "./lib/file";
 import cloneDeep from "lodash.clonedeep";
-import { AppDeploymentFlags } from "@algo-builder/runtime/build/types";
+import { AccountAddress, AppDeploymentFlags, ExecutionMode } from "@algo-builder/runtime/build/types";
+import JSON5 from "json5";
 
 export class TealRuntime {
 
@@ -68,6 +69,10 @@ export class TealRuntime {
     // Configures the algo-builder interpreter and parses the TEAL code to be debugged.
     //
     private configureInterpreter(configuration: any, tealCode: string) {
+
+        //
+        // Load accounts from configuration.
+        //
         let accountMap = new Map<string, AccountStore>();
         let accounts: AccountStore[] = [];
         for (let accountName of Object.keys(configuration.accounts)) {
@@ -77,53 +82,63 @@ export class TealRuntime {
             accounts.push(account);
         }
 
-        const txnParameters: any = configuration.transactionParams;
-        if (txnParameters.fromAccount) {
-            const account = accountMap.get(txnParameters.fromAccount);
-            if (!account) {
-                throw new Error(`Failed to find account ${txnParameters.fromAccount} in the configuration.`);
-            }
-            txnParameters.fromAccountAddr = account.address;
-            delete txnParameters.fromAccount;
-        }
+        // 
+        // Resolve named accounts in transactions.
+        //
 
-        if (txnParameters.toAccount) {
-            const account = accountMap.get(txnParameters.toAccount);
-            if (!account) {
-                throw new Error(`Failed to find account ${txnParameters.toAccount} in the configuration.`);
+        if (configuration.txns) {
+            for (const txn of configuration.txns) {
+                this.resolveAddress(txn, "fromAccount", accountMap);
+                this.resolveAddress(txn, "toAccount", accountMap);
             }
-            txnParameters.toAccountAddr = account.address;
-            delete txnParameters.toAccount;
         }
 
         const runtime = new Runtime(accounts);
 
-        if (txnParameters.sign === webTypes.SignType.LogicSignature) {
-            const lsig = runtime.getLogicSig(tealCode, configuration.logSigArgs || []);
-            const lsigAccount = runtime.getAccount(lsig.address());
-            accountMap.set("$lsig", lsigAccount);
+        //
+        // Load transactions.
+        //
+        const txns: webTypes.ExecParams[] = [];
+
+        if (configuration.txns) {
+            for (const txnConfig of configuration.txns) {
+                txns.push(this.createTransaction(txnConfig, runtime, accountMap));
+            }
         }
 
-        const [tx, gtxs] = runtime.createTxnContext([txnParameters]);
+        //
+        // Load app arguments.
+        //
+        const appArgs: Uint8Array[] = [];
+
+        if (configuration.args) {
+            for (const argConfig of configuration.args) {
+                appArgs.push(this.parseArg(argConfig, accountMap));
+            }
+        }
+
+        const [tx, gtxs] = runtime.createTxnContext(txns);
         runtime.validateTxRound(gtxs);
 
         const store = (runtime as any).store; //TODO: This is a bit ugly. Need to make a change to algo-builder/runtime if we need to keep this.
-        runtime.ctx = new Ctx(cloneDeep(store), tx, gtxs, configuration.appArgs || [], runtime, undefined);
+        runtime.ctx = new Ctx(cloneDeep(store), tx, gtxs, appArgs, runtime, undefined);
 
-        runtime.ctx.tx = runtime.ctx.gtxs[0];
+        const txnIndex = configuration.groupIndex || 0;
+        runtime.ctx.tx = runtime.ctx.gtxs[txnIndex];
 
         runtime.ctx.verifyMinimumFees();
 
-        runtime.ctx.deductFee(txnParameters.fromAccountAddr, 0, txnParameters.payFlags);
+        const currentTxn = txns[txnIndex];       
+        runtime.ctx.deductFee(currentTxn.fromAccountAddr!, 0, currentTxn.payFlags);
 
-        if (txnParameters.type === webTypes.TransactionType.DeployApp) {
-            const senderAcc = runtime.ctx.getAccount(txnParameters.fromAccountAddr);
+        if (currentTxn.type === webTypes.TransactionType.DeployApp) {
+            const senderAcc = runtime.ctx.getAccount(currentTxn.fromAccountAddr!);
             const flags: AppDeploymentFlags = {
                 sender: senderAcc.account,
-                localInts: txnParameters.localInts,
-                localBytes: txnParameters.localBytes,
-                globalInts: txnParameters.globalInts,
-                globalBytes: txnParameters.globalBytes
+                localInts: currentTxn.localInts,
+                localBytes: currentTxn.localBytes,
+                globalInts: currentTxn.globalInts,
+                globalBytes: currentTxn.globalBytes
             };
 
             //
@@ -137,10 +152,140 @@ export class TealRuntime {
 
         this.interpreter = new Interpreter();
         this.interpreter.runtime = runtime;
-        this.interpreter.instructions = parser(tealCode, configuration.mode, this.interpreter);
+        this.interpreter.instructions = parser(tealCode, this.convertMode(configuration.mode), this.interpreter);
         this.interpreter.mapLabelWithIndexes();
     }
 
+    //
+    // Resolves an address by name in a transactions.
+    //
+    private resolveAddress(txn: any, fieldName: string, accountMap: Map<string, AccountStore>) {
+        const accountName = txn && txn[fieldName];
+        if (accountName !== undefined) {
+            const account = accountMap.get(accountName);
+            if (!account) {
+                throw new Error(`Failed to find account ${accountName} in the configuration.`);
+            }
+            txn[`${fieldName}Addr`] = account.address;
+            delete txn[fieldName];
+        }
+    }
+
+    //
+    // Parses an argument for use by algo-builder/runtime.
+    //
+    private parseArg(argConfig: any, accountMap: Map<string, AccountStore>): Uint8Array {
+        switch (argConfig.type) {
+            case "int": {
+                return parsing.uint64ToBigEndian(argConfig.value);
+            }
+
+            case "str": {
+                return parsing.stringToBytes(argConfig.value);
+            }
+
+            case "addr": {
+                const account = accountMap.get(argConfig.value);
+                if (!account) {
+                    throw new Error(`Failed to find account ${argConfig.value} request in "args".`);
+                }
+
+                return parsing.addressToPk(account.account.addr);                                    
+            }
+
+            default: {
+                throw new Error(`Unexpected arg type ${argConfig.type}`);
+            }
+        }
+    }
+
+    //
+    // Creates a algo-builder/runtime transaction from a configuration object.
+    //
+    private createTransaction(txnConfig: any, runtime: Runtime, accountMap: Map<string, AccountStore>): webTypes.ExecParams {
+        switch (txnConfig.type) {
+            case "appl": {
+                let sign: webTypes.SignType;
+                if (txnConfig.sign === "secretkey") {
+                    sign = webTypes.SignType.SecretKey;
+                }
+                else if (txnConfig.sign === "logicsig") {
+                    sign = webTypes.SignType.LogicSignature;
+                }
+                else {
+                    throw new Error(`Unexpected signature type "${txnConfig.sign}", should be one of "secretkey" or "logicsig"`);
+                }
+
+                if (!txnConfig.fromAccountAddr) {
+                    throw new Error(`fromAccount/fromAccountAddr is required`);
+                }
+
+                if (!txnConfig.apid) {
+                    const fromAccount = runtime.getAccount(txnConfig.fromAccountAddr);
+                    if (!fromAccount) {
+                        throw new Error(`Failed to find from account "${txnConfig.fromAccountAddr}`);
+                    }
+
+                    const txn: any = { // It's too difficult to type this with "DeployAppParams".
+                        type: webTypes.TransactionType.DeployApp,
+                        sign: sign,
+                        fromAccountAddr: txnConfig.fromAccountAddr,
+                        lsig: runtime.getLogicSig("int 1", []), //todo: is this needed?
+                        approvalProgram: txnConfig.approvalProgram || "",
+                        clearProgram: txnConfig.clearProgram || "",
+                        localInts: txnConfig.localInts || 1,
+                        localBytes: txnConfig.localBytes || 1,
+                        globalInts: txnConfig.globalInts || 1,
+                        globalBytes: txnConfig.globalBytes || 1,
+                        //TODO: Options from AppOptionalFlags could go here.
+                        payFlags: txnConfig.payFlags,
+                    };
+                    return txn;
+                }
+                else {
+                    if (!txnConfig.appId) {
+                        throw new Error(`appId is required`);
+                    }
+
+                    const txn: any = { // It's too difficult to type this as "AppCallsParam".
+                        type: webTypes.TransactionType.CallApp,
+                        appId: txnConfig.appId as number,
+                        sign: sign,
+                        fromAccountAddr: txnConfig.fromAccountAddr,
+                        lsig: runtime.getLogicSig("", []),
+                        //TODO: Options from AppOptionalFlags could go here.
+                        payFlags: txnConfig.payFlags || {},
+                    };
+                    return txn;
+                }
+            }
+        
+            default: {
+                throw new Error(`Unhandled transaction type ${txnConfig.type}`);
+            }
+        }
+    }
+
+    //
+    // Converts a string to an algo-builder/runtime execution mode.
+    //
+    private convertMode(mode: string | undefined): ExecutionMode {
+        if (!mode) {
+            throw new Error(`TEAL execution mode not supplied, should be one of one of "signature" or "application"`);
+        }
+
+        if (mode === "signature") {
+            return ExecutionMode.SIGNATURE;
+        }
+        else if (mode === "application") {
+            return ExecutionMode.APPLICATION;
+        }
+        else {
+            throw new Error(`Unknown TEAL execution mode "${mode}, expected one of "signature" or "application"`);
+        }
+    }
+
+   
     //
     // Helper function to create an account "configuration" for Algo-builder/runtime.
     //
@@ -161,7 +306,7 @@ export class TealRuntime {
     private async loadConfiguration(tealFilePath: string) {
         const configFilePath = tealFilePath + ".json";
         try {
-            return JSON.parse(await readFile(configFilePath));
+            return JSON5.parse(await readFile(configFilePath));
         }
         catch (err: any) {
             const msg = `Failed to load TEAL debugger configuration file: ${configFilePath}`;
